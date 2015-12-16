6c0ca87bd49c60ec5e83d86dcfede45d4b2c28a1
William Hurley <whurley@forumone.com>
2015-12-16 14:25:06 -0500

    Updating to 0.12.0-rc3 to update Express to 3.20 to avoid static file issue.

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,8 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
+  'GET /api/content/:page' : 'ContentController.getContent',
 
   /***************************************************************************
   *                                                                          *
